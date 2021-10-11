ad75b9c84dc518d7caf6e6fa27a3462b442d0f3c
Aki Salmi <aki.salmi@iki.fi>
2021-10-11 17:06:47 +0300

    :tada: For 'Magic Tricks of Testing', start with production code

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,3 +1,17 @@
 export function calculate(): number {
     return 1
 }
+
+export class Changer {
+    private _availableCoins: number[] = [200, 100, 50, 20, 10, 5]
+    public change(cents: number): number[] {
+        const result = []
+        for (const c of this._availableCoins){
+            while ( cents >=  c ){
+                result.push(c)
+                cents -= c
+            }
+        }
+        return result
+    }
+}
