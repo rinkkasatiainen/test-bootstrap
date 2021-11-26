eadeda6b374585df6af0b64fe9a58dc2c0b214f1
Aki Salmi <aki.salmi@iki.fi>
2021-11-26 10:28:37 +0200

    Adding a test list

---
+++ test/mocha/can-do-stuff.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+
+describe('It', () => {
+    it('has nine fields in a 3x3 grid')
+    it('has two players in the game (X and O)')
+    it('allows a player to take an empty field')
+    it('switches player after every turn')
+    describe('ends the game when', () =>  {
+        it('all fields in a row are taken')
+        it('all fields in a column are taken by a player')
+        it('player makes giagonal line')
+        it('all fields are filled')
+    })
+})
