3df2c553038d43d48cd27ee1202b6cf30b85aee6
Carlton <carlton@ilicmcfarlane.io>
2021-12-03 12:18:26 +0000

    :white_check_mark: add solution for callbacks challenge 5

---
+++ test/callbacks/challenge5.spec.ts
@@ -0,0 +1,22 @@
+
+
+// TODO: Fix Typescript compiler errors to make the callback accept a generic type and return that a generic result
+import { expect } from 'chai'
+
+function mapWith(array: number[], callback: (input: unknown) => unknown): unknown[] {
+    const newArray: unknown[] = []
+    // eslint-disable-next-line @typescript-eslint/prefer-for-of
+    array.forEach(item => {
+        newArray.push(callback(item))
+    })
+    return newArray
+}
+
+describe('challenge5', () => {
+    describe('mapWith', () => {
+        it('works', () => {
+            const addTwo: (num: number) => number = (num) => num + 2
+            expect(mapWith([1, 2, 3], addTwo)).to.eql([3, 4, 5])
+        })
+    })
+})
