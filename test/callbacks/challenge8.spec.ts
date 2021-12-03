cc6d2e3bce7e4a00e8fe5d84f4e9e74dd3a72665
Carlton <carlton@ilicmcfarlane.io>
2021-12-03 15:56:44 +0000

    :white_check_mark: add reduce() solution for callbacks challenge 8 - union

--- test/callbacks/challenge8.spec.ts
+++ test/callbacks/challenge8.spec.ts
@@ -2,15 +2,24 @@ import { expect } from 'chai'
 
 // TODO: Is using unknown the right approach?
 // TODO: Figure out why ESLint complains about Set
-// TODO: Find an approach using reduce
 function union(...inputArrays: unknown[][]) {
     return [...new Set(inputArrays.flat())]
 }
 
+function unionWithReduce(...inputArrays: unknown[][]) {
+    return inputArrays.reduce((newArray, currentArray) => newArray
+        .concat(currentArray.filter(element => !newArray.includes(element))))
+}
+
 describe('challenge8', () => {
     describe('union', () => {
-        it('works', () => {
+        it('works using Set', () => {
             expect(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5])).to.eql([5, 10, 15, 88, 1, 7, 100])
+        })
+
+        it('works using reduce', () => {
+            expect(unionWithReduce([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]))
+                .to.eql([5, 10, 15, 88, 1, 7, 100])
         })
     })
 })
