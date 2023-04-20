9f1da520494720be7fff3813884559f29e4dd758
Aki Salmi <aki.salmi@iki.fi>
2023-04-20 16:04:15 +0300

    ✅: Commit 12 - with "full" branch coverage

--- typescript/test/get-price.spec.ts
+++ typescript/test/get-price.spec.ts
@@ -2,33 +2,79 @@ import {expect} from 'chai'
 import {getPrice} from '../src/domain/get-price'
 
 describe('get-price', () => {
+    const aRandomMonday = '2022-02-28'
+    const notMonday = '2022-02-27'
+    const basePrice = 20
+    const getBasePrice = () => Promise.resolve({cost: basePrice})
     describe('a day pass', () => {
         const liftPassType = '1jour'
         describe('when not a holiday', () => {
             const listHolidays = () => Promise.resolve([])
-            const basePrice = 20
-            const getBasePrice = () => Promise.resolve({cost: basePrice})
-
-            const aRandomMonday = '2022-02-28'
-            const notMonday = '2022-02-27'
 
             it('appears to have a discount of 35% on Monday', async () => {
                 const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 21, aRandomMonday)
 
-                const cost = Math.floor(basePrice * 0.65)
+                const cost = Math.ceil(basePrice * 0.65)
                 expect(result).to.eql({cost})
             })
             it('Monday for a child is more expensive than for an adult', async () => {
                 const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 14, aRandomMonday)
 
-                const cost = Math.floor(basePrice * 0.7)
+                const cost = Math.ceil(basePrice * 0.7)
                 expect(result).to.eql({cost})
+            })
+            it('Monday for more than 64y old has bigger reduction', async () => {
+                const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 65, aRandomMonday)
+
+                const cost = Math.ceil(basePrice * 0.65 * 0.75)
+                expect(result).to.eql({cost})
+
             })
             it('typically uses base price', async () => {
                 const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 21, notMonday)
 
                 expect(result).to.eql({cost: basePrice})
             })
+            it('costs nothing for 5yo', async () => {
+                const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 5, aRandomMonday)
+
+                expect(result).to.eql({cost: 0})
+            })
+        })
+        describe('a holiday', () => {
+            const listHolidays = () => Promise.resolve(
+                [{holiday: new Date(aRandomMonday)}, {holiday: new Date(notMonday)}]
+            )
+
+            it('no reduction on holidays, even on Mondays', async () => {
+                const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 21, aRandomMonday)
+
+                const cost = basePrice
+                expect(result).to.eql({cost})
+            })
+        })
+    })
+    describe('night passes', () => {
+        const listHolidays = () => Promise.resolve(
+            [{holiday: new Date(aRandomMonday)}, {holiday: new Date(notMonday)}]
+        )
+        const liftPassType = 'night'
+        it('no reduction on night passes', async () => {
+            const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 21, aRandomMonday)
+
+            const cost = basePrice
+            expect(result).to.eql({cost})
+        })
+        it('reduction for 65yo on night passes', async () => {
+            const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 65, aRandomMonday)
+
+            const cost = Math.ceil(basePrice * 0.4)
+            expect(result).to.eql({cost})
+        })
+        it('free for child < 6', async () => {
+            const result = await getPrice(getBasePrice, listHolidays)(liftPassType, 5, aRandomMonday)
+
+            expect(result).to.eql({cost: 0})
         })
     })
 })
