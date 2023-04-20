70d2f13d6827c87b4ee4d6133797754e34d30ed4
Aki Salmi <aki.salmi@iki.fi>
2023-04-20 15:50:03 +0300

    🎉: Commit 0 - start

--- typescript/index.js
+++ typescript/index.js
@@ -1,7 +1,7 @@
-const { createApp } = require('./lib/src/prices');
+const { createApp } = require('./src/prices');
 
 createApp().
-then(({ app, connection }) => app.listen(5010));
+    then(({ app, connection }) => app.listen(5010));
 
 console.log(`LiftPassPricing Api started on 5010,
 you can open http://localhost:5010/prices?type=night&age=23&date=2019-02-18 in a navigator
