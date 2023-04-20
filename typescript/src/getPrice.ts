8c8161c865192316090e83fa4c171257d560f102
Aki Salmi <aki.salmi@iki.fi>
2023-04-20 16:01:56 +0300

    🚚: Commit 10 - getPrice to own file

---
+++ typescript/src/getPrice.ts
@@ -0,0 +1,73 @@
+// Domain concepts - value objects
+export interface TicketPrice {
+    cost: number;
+}
+
+interface Holiday {
+    getFullYear: () => number;
+    getMonth: () => number;
+    getDate: () => number;
+}
+
+// Repository functions
+export type GetBasePrice = (liftPassType: string) => Promise<TicketPrice>
+export type GetHolidays = () => Promise<Array<{ holiday: Holiday }>>
+// Domain functions
+type CalculatesPrice = (liftPassType: string, age: number, date: string) => Promise<TicketPrice>
+export const getPrice:
+    (basePriceFor: GetBasePrice, listHolidays: GetHolidays) => CalculatesPrice =
+    (basePriceFor, listHolidays) => async (liftPassType, age, date) => {
+        if (age < 6) {
+            return {cost: 0}
+        } else {
+            const basePrice: TicketPrice = await basePriceFor(liftPassType)
+
+            if (liftPassType !== 'night') {
+                const holidays = await listHolidays()
+
+                let isHoliday
+                let reduction = 0
+                for (const row of holidays) {
+                    // eslint-disable-next-line max-len
+                    const holiday = row.holiday as unknown as Holiday
+                    if (date) {
+                        const d = new Date(date)
+                        if (d.getFullYear() === holiday.getFullYear()
+                            && d.getMonth() === holiday.getMonth()
+                            && d.getDate() === holiday.getDate()) {
+                            isHoliday = true
+                        }
+                    }
+                }
+                if (!isHoliday && new Date(date).getDay() === 1) {
+                    reduction = 35
+                }
+                if (age < 15) {
+                    return {cost: Math.ceil(basePrice.cost * .7)}
+                } else {
+                    if (age === undefined) {
+                        const cost = basePrice.cost * (1 - reduction / 100)
+                        return {cost: Math.ceil(cost)}
+                    } else {
+                        if (age > 64) {
+                            const cost = basePrice.cost * .75 * (1 - reduction / 100)
+                            return {cost: Math.ceil(cost)}
+                        } else {
+                            const cost = basePrice.cost * (1 - reduction / 100)
+                            return {cost: Math.ceil(cost)}
+                        }
+                    }
+                }
+            } else {
+                if (age >= 6) {
+                    if (age > 64) {
+                        return {cost: Math.ceil(basePrice.cost * .4)}
+                    } else {
+                        return basePrice
+                    }
+                } else {
+                    return {cost: 0}
+                }
+            }
+        }
+    }
--- typescript/src/prices.ts
+++ typescript/src/prices.ts
@@ -1,22 +1,6 @@
 import express from 'express'
 import mysql, {Connection} from 'mysql2/promise'
-
-interface TicketPrice {
-    cost: number;
-}
-
-interface Holiday {
-    getFullYear: () => number;
-    getMonth: () => number;
-    getDate: () => number;
-}
-
-// Repository functions
-type GetBasePrice = (liftPassType: string) => Promise<TicketPrice>
-type GetHolidays = () => Promise<Array<{ holiday: Holiday }>>
-
-// Domain functions
-type CalculatesPrice = (liftPassType: string, age: number, date: string) => Promise<TicketPrice>
+import {GetBasePrice, GetHolidays, getPrice, TicketPrice} from './getPrice'
 
 const getBasePrice: (conn: Connection) => GetBasePrice =
     // @ts-ignore
@@ -30,64 +14,6 @@ const getHolidays: (conn: Connection) => GetHolidays =
     conn => async () => (await conn.query(
         'SELECT * FROM `holidays`'
     ))[0] as mysql.RowDataPacket[]
-
-const getPrice:
-    (basePriceFor: GetBasePrice, listHolidays: GetHolidays) => CalculatesPrice =
-    (basePriceFor, listHolidays) => async (liftPassType, age, date) => {
-        if (age < 6) {
-            return {cost: 0}
-        } else {
-            const basePrice: TicketPrice = await basePriceFor(liftPassType)
-
-            if (liftPassType !== 'night') {
-                const holidays = await listHolidays()
-
-                let isHoliday
-                let reduction = 0
-                for (const row of holidays) {
-                    // eslint-disable-next-line max-len
-                    const holiday = row.holiday as unknown as Holiday
-                    if (date) {
-                        const d = new Date(date)
-                        if (d.getFullYear() === holiday.getFullYear()
-                            && d.getMonth() === holiday.getMonth()
-                            && d.getDate() === holiday.getDate()) {
-                            isHoliday = true
-                        }
-                    }
-                }
-                if (!isHoliday && new Date(date).getDay() === 1) {
-                    reduction = 35
-                }
-                if (age < 15) {
-                    return {cost: Math.ceil(basePrice.cost * .7)}
-                } else {
-                    if (age === undefined) {
-                        const cost = basePrice.cost * (1 - reduction / 100)
-                        return {cost: Math.ceil(cost)}
-                    } else {
-                        if (age > 64) {
-                            const cost = basePrice.cost * .75 * (1 - reduction / 100)
-                            return {cost: Math.ceil(cost)}
-                        } else {
-                            const cost = basePrice.cost * (1 - reduction / 100)
-                            return {cost: Math.ceil(cost)}
-                        }
-                    }
-                }
-            } else {
-                if (age >= 6) {
-                    if (age > 64) {
-                        return {cost: Math.ceil(basePrice.cost * .4)}
-                    } else {
-                        return basePrice
-                    }
-                } else {
-                    return {cost: 0}
-                }
-            }
-        }
-    }
 
 async function createApp() {
     const app = express()
