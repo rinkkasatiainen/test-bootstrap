afe87195f0fee9510979588085b79a1bcc8eb63c
Aki Salmi <aki.salmi@iki.fi>
2023-04-20 15:59:56 +0300

    🎨: Commit 9 - introducing a pure function for domain logic.

--- typescript/src/prices.ts
+++ typescript/src/prices.ts
@@ -28,6 +28,65 @@ const getHolidays: (conn: Connection) => GetHolidays =
         'SELECT * FROM `holidays`'
     ))[0] as mysql.RowDataPacket[]
 
+const getPrice:
+    (basePriceFor: GetBasePrice, listHolidays: GetHolidays, liftPassType: string, age: number, date: string) =>
+        Promise<BasePrice> =
+    async (basePriceFor, listHolidays,  liftPassType, age, date) => {
+        if (age < 6) {
+            return {cost: 0}
+        } else {
+            const basePrice: BasePrice = await basePriceFor(liftPassType)
+
+            if (liftPassType !== 'night') {
+                const holidays = await listHolidays()
+
+                let isHoliday
+                let reduction = 0
+                for (const row of holidays) {
+                    // eslint-disable-next-line max-len
+                    const holiday = row.holiday as unknown as Holiday
+                    if (date) {
+                        const d = new Date(date)
+                        if (d.getFullYear() === holiday.getFullYear()
+                            && d.getMonth() === holiday.getMonth()
+                            && d.getDate() === holiday.getDate()) {
+                            isHoliday = true
+                        }
+                    }
+                }
+                if (!isHoliday && new Date(date).getDay() === 1) {
+                    reduction = 35
+                }
+                if (age < 15) {
+                    return {cost: Math.ceil(basePrice.cost * .7)}
+                } else {
+                    if (age === undefined) {
+                        const cost = basePrice.cost * (1 - reduction / 100)
+                        return {cost: Math.ceil(cost)}
+                    } else {
+                        if (age > 64) {
+                            const cost = basePrice.cost * .75 * (1 - reduction / 100)
+                            return {cost: Math.ceil(cost)}
+                        } else {
+                            const cost = basePrice.cost * (1 - reduction / 100)
+                            return {cost: Math.ceil(cost)}
+                        }
+                    }
+                }
+            } else {
+                if (age >= 6) {
+                    if (age > 64) {
+                        return {cost: Math.ceil(basePrice.cost * .4)}
+                    } else {
+                        return basePrice
+                    }
+                } else {
+                    return {cost: 0}
+                }
+            }
+        }
+    }
+
 async function createApp() {
     const app = express()
 
@@ -62,60 +121,8 @@ async function createApp() {
         // @ts-ignore
         const date: string = req.query.date
 
-        let result: { cost: number } | undefined
-        if (age < 6) {
-            result = {cost: 0}
-        } else {
-            const basePrice: BasePrice = await basePriceFor(liftPassType)
-
-            if (liftPassType !== 'night') {
-                const holidays = await listHolidays()
-
-                let isHoliday
-                let reduction = 0
-                for (const row of holidays) {
-                    // eslint-disable-next-line max-len
-                    const holiday = row.holiday as unknown as Holiday
-                    if (date) {
-                        const d = new Date(date)
-                        if (d.getFullYear() === holiday.getFullYear()
-                            && d.getMonth() === holiday.getMonth()
-                            && d.getDate() === holiday.getDate()) {
-                            isHoliday = true
-                        }
-                    }
-                }
-                if (!isHoliday && new Date(date).getDay() === 1) {
-                    reduction = 35
-                }
-                if (age < 15) {
-                    result = {cost: Math.ceil(basePrice.cost * .7)}
-                } else {
-                    if (age === undefined) {
-                        const cost = basePrice.cost * (1 - reduction / 100)
-                        result = {cost: Math.ceil(cost)}
-                    } else {
-                        if (age > 64) {
-                            const cost = basePrice.cost * .75 * (1 - reduction / 100)
-                            result = {cost: Math.ceil(cost)}
-                        } else {
-                            const cost = basePrice.cost * (1 - reduction / 100)
-                            result = {cost: Math.ceil(cost)}
-                        }
-                    }
-                }
-            } else {
-                if (age >= 6) {
-                    if (age > 64) {
-                        result = {cost: Math.ceil(basePrice.cost * .4)}
-                    } else {
-                        result = basePrice
-                    }
-                } else {
-                    result = {cost: 0}
-                }
-            }
-        }
+        // This looks like a Pure Function that has some domain logic.
+        const result: BasePrice  = await getPrice(basePriceFor, listHolidays, liftPassType, age, date)
         res.json(result)
     })
     return {app, connection}
